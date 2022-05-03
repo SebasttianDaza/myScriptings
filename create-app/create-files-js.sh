d87679056434e8279afac226660fc647f56c767a
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-05-03 08:54:52 -0500

    Added eslint and half finished the configuration

--- create-app/create-files-js.sh
+++ create-app/create-files-js.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "Name the your project:"
+echo "Name the your project 😎 :"
 read  nameProject
-echo "Type of template"
+echo "Type of template 💻"
 read template
+echo "You want this project with eslint 🎁 "
+read eslint
 
 npm create vite@latest $nameProject -- --template $template 
 
@@ -18,7 +20,19 @@ function configReact(){
 	touch README.md
 	mkdir Public && mkdir Public/Images && mv src/favicon.svg  ./Public/Images && mv src/logo.svg ./Public/Images
 	mkdir src/Hooks && mkdir src/Services && mkdir src/Components
-	mkdir src/Errors && 
+	mkdir src/Errors 
+
+	npm i
+
+	if [[ $eslint == "yes" ]]
+	then 
+		cat /home/sebasttiandaza/myScripting/eslint/React/.eslintrc.js > .eslintrc.js
+		npm i eslint --save-dev
+		npm i --save-dev eslint-config-prettier
+		npm i eslint-plugin-react-hooks
+	fi
+
+	code .
 }
 
 if [[ $template == "react" ]]
