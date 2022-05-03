e648115bf45f420e15e9ae90327dad8275507518
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-05-03 08:03:49 -0500

    Added Readme and also I've already started the automation
    I've been working on this

--- create-app/create-files-js.sh
+++ create-app/create-files-js.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 echo "Name the your project:"
 read  nameProject
+echo "Type of template"
+read template
 
-npm create vite@latest $nameProject --template vanilla  
+npm create vite@latest $nameProject -- --template $template 
 
-echo "Finish, Bye" 
+function configVanilla(){ 
+	echo "Hola Vanilla"
+	cd $nameProject
+	touch README.md
+}
+
+function configReact(){
+	echo "We're starting this process, One moment"
+	cd $nameProject
+	touch README.md
+	mkdir Public && mkdir Public/Images && mv src/favicon.svg  ./Public/Images && mv src/logo.svg ./Public/Images
+	mkdir src/Hooks && mkdir src/Services && mkdir src/Components
+	mkdir src/Errors && 
+}
+
+if [[ $template == "react" ]]
+then
+	configReact
+fi
+
+if [[ $template == "vanilla" ]]
+then
+	configVanilla
+fi
+
+echo "Finished, Bye"
